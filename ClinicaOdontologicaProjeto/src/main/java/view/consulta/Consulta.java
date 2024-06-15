d840c9b13e221961227a8c08406ed6b051b3df68
FelipeBragaValotaEsteves <802.400@alunos.unigran.br>
2024-06-14 21:46:16 -0400

    Correcao da tela de listagem

--- ClinicaOdontologicaProjeto/src/main/java/view/consulta/Consulta.java
+++ ClinicaOdontologicaProjeto/src/main/java/view/consulta/Consulta.java
@@ -2,6 +2,7 @@ package view.consulta;
 
 import controller.Controller;
 import dto.DTO;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.util.List;
 import javax.swing.JButton;
@@ -12,7 +13,7 @@ import javax.swing.table.DefaultTableModel;
 import view.cadastro.Cadastro;
 import view.cadastro.CadastroInterface;
 
-public class Consulta extends javax.swing.JDialog {
+public class Consulta extends javax.swing.JFrame {
 
     CadastroInterface cadastroInterface;
     List<DTO> lista;
@@ -31,145 +32,133 @@ public class Consulta extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton4 = new javax.swing.JButton();
         jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
-        jPanel3 = new javax.swing.JPanel();
         btnFechar = new javax.swing.JButton();
         btnNovo = new javax.swing.JButton();
         btnEditar = new javax.swing.JButton();
         btnRemover = new javax.swing.JButton();
+        btnPesquisar = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel3 = new javax.swing.JPanel();
         panelFiltros = new javax.swing.JPanel();
         jPanel4 = new javax.swing.JPanel();
         jPanel6 = new javax.swing.JPanel();
         jPanel7 = new javax.swing.JPanel();
-        btnPesquisar = new javax.swing.JButton();
-        panelFiltrosIn = new javax.swing.JPanel();
         jPanel5 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jScrollPane2 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
 
-        jButton4.setText("Editar");
-        jButton4.setToolTipText("");
-        jButton4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximumSize(new java.awt.Dimension(650, 470));
         setMinimumSize(new java.awt.Dimension(650, 470));
-        setModal(true);
-        setResizable(false);
+        setPreferredSize(new java.awt.Dimension(650, 470));
         setSize(new java.awt.Dimension(650, 470));
 
         jPanel1.setBackground(new java.awt.Color(243, 102, 186));
-        jPanel1.setMaximumSize(null);
-        jPanel1.setPreferredSize(new java.awt.Dimension(12, 267));
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 12, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 418, Short.MAX_VALUE)
-        );
-
-        getContentPane().add(jPanel1, java.awt.BorderLayout.LINE_END);
-
-        jPanel2.setBackground(new java.awt.Color(243, 102, 186));
-        jPanel2.setPreferredSize(new java.awt.Dimension(12, 267));
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 12, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 418, Short.MAX_VALUE)
-        );
-
-        getContentPane().add(jPanel2, java.awt.BorderLayout.LINE_START);
-
-        jPanel3.setBackground(new java.awt.Color(243, 102, 186));
+        jPanel1.setMaximumSize(new java.awt.Dimension(50, 50));
+        jPanel1.setMinimumSize(new java.awt.Dimension(50, 50));
+        jPanel1.setPreferredSize(new java.awt.Dimension(50, 50));
 
         btnFechar.setText("Fechar");
-        btnFechar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnFechar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnFecharActionPerformed(evt);
             }
         });
-        jPanel3.add(btnFechar);
+        jPanel1.add(btnFechar);
 
         btnNovo.setText("Novo");
-        btnNovo.setToolTipText("");
-        btnNovo.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnNovo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnNovoActionPerformed(evt);
             }
         });
-        jPanel3.add(btnNovo);
+        jPanel1.add(btnNovo);
 
         btnEditar.setText("Editar");
-        btnEditar.setToolTipText("");
-        btnEditar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnEditar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnEditarActionPerformed(evt);
             }
         });
-        jPanel3.add(btnEditar);
+        jPanel1.add(btnEditar);
 
         btnRemover.setText("Remover\n");
-        btnRemover.setToolTipText("");
-        btnRemover.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         btnRemover.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnRemoverActionPerformed(evt);
             }
         });
-        jPanel3.add(btnRemover);
+        jPanel1.add(btnRemover);
 
-        getContentPane().add(jPanel3, java.awt.BorderLayout.PAGE_END);
+        btnPesquisar.setText("Pesquisar");
+        btnPesquisar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPesquisarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnPesquisar);
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.PAGE_END);
+
+        jPanel2.setBackground(new java.awt.Color(243, 102, 186));
+        jPanel2.setMaximumSize(new java.awt.Dimension(50, 50));
+        jPanel2.setMinimumSize(new java.awt.Dimension(50, 50));
+        jPanel2.setPreferredSize(new java.awt.Dimension(50, 50));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 50, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 370, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(jPanel2, java.awt.BorderLayout.LINE_START);
+
+        jPanel3.setBackground(new java.awt.Color(243, 102, 186));
+        jPanel3.setMaximumSize(new java.awt.Dimension(50, 50));
+        jPanel3.setMinimumSize(new java.awt.Dimension(50, 50));
+        jPanel3.setPreferredSize(new java.awt.Dimension(50, 50));
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 50, Short.MAX_VALUE)
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 370, Short.MAX_VALUE)
+        );
+
+        getContentPane().add(jPanel3, java.awt.BorderLayout.LINE_END);
 
         panelFiltros.setBackground(new java.awt.Color(243, 102, 186));
         panelFiltros.setLayout(new java.awt.BorderLayout());
 
         jPanel4.setBackground(new java.awt.Color(243, 102, 186));
+        jPanel4.setMinimumSize(new java.awt.Dimension(50, 50));
         jPanel4.setPreferredSize(new java.awt.Dimension(50, 50));
 
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
         jPanel4.setLayout(jPanel4Layout);
         jPanel4Layout.setHorizontalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-<<<<<<< HEAD
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
-                .addContainerGap(260, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(257, 257, 257))
-        );
-        jPanel4Layout.setVerticalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
-                .addContainerGap(37, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(24, 24, 24))
-=======
             .addGap(0, 50, Short.MAX_VALUE)
         );
         jPanel4Layout.setVerticalGroup(
             jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 50, Short.MAX_VALUE)
->>>>>>> main
         );
 
         panelFiltros.add(jPanel4, java.awt.BorderLayout.LINE_END);
 
         jPanel6.setBackground(new java.awt.Color(243, 102, 186));
+        jPanel6.setMinimumSize(new java.awt.Dimension(50, 50));
         jPanel6.setPreferredSize(new java.awt.Dimension(50, 50));
 
         javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
@@ -186,35 +175,13 @@ public class Consulta extends javax.swing.JDialog {
         panelFiltros.add(jPanel6, java.awt.BorderLayout.LINE_START);
 
         jPanel7.setBackground(new java.awt.Color(243, 102, 186));
-        jPanel7.setMaximumSize(new java.awt.Dimension(100, 100));
-        jPanel7.setPreferredSize(new java.awt.Dimension(30, 30));
-        jPanel7.setRequestFocusEnabled(false);
-        jPanel7.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        btnPesquisar.setText("Pesquisar\n");
-        btnPesquisar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnPesquisarActionPerformed(evt);
-            }
-        });
-        jPanel7.add(btnPesquisar, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 0, 140, 30));
-
-        panelFiltros.add(jPanel7, java.awt.BorderLayout.PAGE_END);
-
-        panelFiltrosIn.setBackground(new java.awt.Color(243, 102, 186));
-        panelFiltrosIn.setForeground(new java.awt.Color(255, 255, 255));
-        panelFiltrosIn.setLayout(new java.awt.GridLayout(5, 4, 0, 4));
-        panelFiltros.add(panelFiltrosIn, java.awt.BorderLayout.CENTER);
+        jPanel7.setLayout(new java.awt.GridLayout(5, 8));
+        panelFiltros.add(jPanel7, java.awt.BorderLayout.CENTER);
 
         getContentPane().add(panelFiltros, java.awt.BorderLayout.PAGE_START);
 
         jPanel5.setBackground(new java.awt.Color(243, 102, 186));
 
-        jScrollPane1.setBackground(new java.awt.Color(244, 136, 204));
-
-        jTable1.setBackground(new java.awt.Color(243, 102, 186));
-        jTable1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jTable1.setForeground(new java.awt.Color(255, 255, 255));
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
@@ -223,24 +190,19 @@ public class Consulta extends javax.swing.JDialog {
 
             }
         ));
-        jScrollPane2.setViewportView(jTable1);
-
-        jScrollPane1.setViewportView(jScrollPane2);
+        jScrollPane1.setViewportView(jTable1);
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
         jPanel5Layout.setHorizontalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel5Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 657, Short.MAX_VALUE)
-                .addContainerGap())
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 550, Short.MAX_VALUE)
         );
         jPanel5Layout.setVerticalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel5Layout.createSequentialGroup()
-                .addGap(0, 58, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 360, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 364, Short.MAX_VALUE))
         );
 
         getContentPane().add(jPanel5, java.awt.BorderLayout.CENTER);
@@ -258,9 +220,9 @@ public class Consulta extends javax.swing.JDialog {
         }
     }//GEN-LAST:event_btnRemoverActionPerformed
 
-    private void btnFecharActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFecharActionPerformed
-        dispose();
-    }//GEN-LAST:event_btnFecharActionPerformed
+    private void btnNovoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNovoActionPerformed
+        new Cadastro(null, true, cadastroInterface, controller).setVisible(true);
+    }//GEN-LAST:event_btnNovoActionPerformed
 
     private void btnEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditarActionPerformed
         try {
@@ -270,12 +232,11 @@ public class Consulta extends javax.swing.JDialog {
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Não foi possível editar este item!", "Error", JOptionPane.ERROR_MESSAGE);
         }
-
     }//GEN-LAST:event_btnEditarActionPerformed
 
-    private void btnNovoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNovoActionPerformed
-
-    }//GEN-LAST:event_btnNovoActionPerformed
+    private void btnFecharActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnFecharActionPerformed
+        dispose();
+    }//GEN-LAST:event_btnFecharActionPerformed
 
     private void btnPesquisarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPesquisarActionPerformed
         try {
@@ -302,12 +263,12 @@ public class Consulta extends javax.swing.JDialog {
             JLabel label = new JLabel(title);
             label.setForeground(Color.WHITE);
             JTextField field = new JTextField(20);
-            panelFiltrosIn.add(label);
-            panelFiltrosIn.add(field);
+            jPanel7.add(label, BorderLayout.CENTER);
+            jPanel7.add(field, BorderLayout.CENTER);
         }
 
-        panelFiltrosIn.revalidate();
-        panelFiltrosIn.repaint();
+        jPanel7.revalidate();
+        jPanel7.repaint();
     }
 
     private void btnPesquisar(java.awt.event.ActionEvent evt) {
@@ -318,13 +279,13 @@ public class Consulta extends javax.swing.JDialog {
         }
     }
 
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnEditar;
     private javax.swing.JButton btnFechar;
     private javax.swing.JButton btnNovo;
     private javax.swing.JButton btnPesquisar;
     private javax.swing.JButton btnRemover;
-    private javax.swing.JButton jButton4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
@@ -333,9 +294,7 @@ public class Consulta extends javax.swing.JDialog {
     private javax.swing.JPanel jPanel6;
     private javax.swing.JPanel jPanel7;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTable jTable1;
     private javax.swing.JPanel panelFiltros;
-    private javax.swing.JPanel panelFiltrosIn;
     // End of variables declaration//GEN-END:variables
 }
--- ClinicaOdontologicaProjeto/target/generated-sources/annotations/model/Material_.java
+++ ClinicaOdontologicaProjeto/target/generated-sources/annotations/model/Material_.java
@@ -4,11 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-11T00:31:24", comments="EclipseLink-2.7.12.v20230209-rNA")
-=======
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-14T12:39:59", comments="EclipseLink-2.7.12.v20230209-rNA")
->>>>>>> main
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-06-14T21:45:11", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Material.class)
 public class Material_ { 
 
